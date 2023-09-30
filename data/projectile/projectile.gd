7277c56e9d7a9fa3bcfd9a006c4db82b387a9f38
Junis Milori <junis.milori@gmail.com>
2023-10-01 00:59:34 +0200

    Added timer for bullets

--- data/projectile/projectile.gd
+++ data/projectile/projectile.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 @onready var mesh1 = $MeshInstance3D
 @onready var mesh2 = $MeshInstance3D2
 @onready var rayCast = $RayCast3D
+@onready var freeQueueTimer = $freeQueueTimer
 
 const SPEED = 60
 
@@ -14,5 +15,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	if freeQueueTimer.is_stopped():
+		self.queue_free()
 #	var rayCastVector = Vector3(rayCast.get_position().x, rayCast.get_position().y, rayCast.get_position().z)
 #	move_and_collide(rayCast.position)
